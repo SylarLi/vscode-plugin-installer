aad606cfb0e93d95e9e4afe26d3e90339dda9e04
lilu <li.lu@cloudcade.com>
2025-03-05 18:44:29 +0800

    skip download pre-release version

--- extension.js
+++ extension.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const https = require('https');
 const os = require('os');
 const zlib = require('zlib');
+const jszip = require('jszip');
+
+var vsixs = [];
 
 // Activation function, plugin entry
 function activate(context) {
@@ -38,7 +41,8 @@ function activate(context) {
 
         const extension = await vscode.extensions.getExtension(pluginId);
         if (extension) {
-            throw new Error(`Plugin ${pluginId} is already installed!`);
+            vscode.window.showErrorMessage(`Plugin ${pluginId} is already installed!`);
+            return;
         }
 
         try {
@@ -50,7 +54,8 @@ function activate(context) {
                 cancellable: false
             }, async (progress) => {
                 progress.report({ message: 'Starting installation...' });
-                await installPlugin(pluginId);
+                await downloadPlugin(pluginId);
+                await setupPlugins();
                 progress.report({ message: 'Finishing installation...', increment: 100 });
             });
             vscode.window.showInformationMessage(`Plugin ${pluginId} installed successfully!`);
@@ -62,8 +67,8 @@ function activate(context) {
     context.subscriptions.push(disposable);
 }
 
-// Get plugin version information
-async function getPluginVersion(pluginId) {
+// Get plugin information
+async function getLatestVersion(pluginId) {
     const apiUrl = `https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery`;
     const response = await fetch(apiUrl, {
         method: 'POST',
@@ -77,33 +82,46 @@ async function getPluginVersion(pluginId) {
                     { filterType: 7, value: pluginId }
                 ]
             }],
-            flags: 914
+            pageNumber: 1,
+            pageSize: 100,
+            flags: 0x1 | 0x10
         })
     });
     const data = await response.json();
-    return data.results[0].extensions[0].versions[0];
+    const firstNonePreReleaseVersion = data.results[0].extensions[0].versions.find(v => !(v.properties?.find(p => p.key === 'Microsoft.VisualStudio.Code.PreRelease')?.value === 'true'));
+    return firstNonePreReleaseVersion.version;
 }
 
 // Plugin installation function
-async function installPlugin(pluginId) {
+async function downloadPlugin(pluginId) {
+    if (vsixs.includes(pluginId)) {
+        const index = vsixs.indexOf(pluginId);
+        vsixs.splice(index, 1);
+        vsixs.push(pluginId);
+        return;
+    }
+
+    const extension = await vscode.extensions.getExtension(pluginId);
+    if (extension) {
+        return;
+    }
+
     const tempDir = path.join(os.tmpdir(), 'vscode-plugins');
     if (!fs.existsSync(tempDir)) {
         fs.mkdirSync(tempDir);
     }
 
-    const vsixPath = path.join(tempDir, `${pluginId}.vsix`);
+    var version = await getLatestVersion(pluginId);
+    const vsixPath = path.join(tempDir, `${pluginId}_${version}.vsix`);
 
-    // Download the VSIX file
     try {
-        // Get latest version number
-        const versionInfo = await getPluginVersion(pluginId);
         await new Promise((resolve, reject) => {
             const file = fs.createWriteStream(vsixPath);
             file.on('error', err => {
                 fs.unlink(vsixPath, () => { });
                 reject(new Error(`Failed to write plugin file: ${err.message}`));
             });
-            const url = `https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${pluginId.split('.')[0]}/vsextensions/${pluginId.split('.')[1]}/${versionInfo.version}/vspackage`;
+            const url = `https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${pluginId.split('.')[0]}/vsextensions/${pluginId.split('.')[1]}/${version}/vspackage`;
             console.log(`Downloading plugin from: ${url}`);
             console.log('Saving plugin to: ' + vsixPath);
             https.get(url, response => {
@@ -131,45 +149,49 @@ async function installPlugin(pluginId) {
         throw new Error(`Plugin download failed: ${error.message}`);
     }
 
-    try {
-        // Install the plugin
-        console.log(`Installing plugin from: ${vsixPath}`);
-        await vscode.commands.executeCommand('workbench.extensions.installExtension', vscode.Uri.file(vsixPath));
-    } catch (error) {
-        throw new Error(`Plugin installation failed: ${error.message}`);
-    }
-    finally {
-        // Clean up
-        fs.unlinkSync(vsixPath);
-    }
+    vsixs.push(vsixPath);
 
-    // 轮询等待插件安装完成
-    const maxAttempts = 3;
-    let attempts = 0;
-    let installedExtension;
-    while (attempts < maxAttempts) {
-        installedExtension = await vscode.extensions.getExtension(pluginId);
-        if (installedExtension) {
-            break;
-        }
-        await new Promise(resolve => setTimeout(resolve, 1000)); // 等待 1 秒
-        attempts++;
+    const dependencies = await getPluginDependencies(vsixPath);
+    for (const dependency of dependencies) {
+        await downloadPlugin(dependency);
     }
+}
 
-    if (!installedExtension) {
-        throw new Error(`Failed to install plugin: ${pluginId}`);
-    }
-
-    // Check and install dependencies
-    const packageJSON = installedExtension.packageJSON;
-    if (packageJSON.extensionDependencies) {
-        for (const dependency of packageJSON.extensionDependencies) {
-            const depExtension = vscode.extensions.getExtension(dependency);
-            if (!depExtension) {
-                vscode.window.showInformationMessage(`Installing dependency: ${dependency}...`);
-                await installPlugin(dependency);
+async function getPluginDependencies(vsixPath) {
+    const dependencies = [];
+    const vsixBuffer = fs.readFileSync(vsixPath);
+    const vsixZip = new jszip();
+    await vsixZip.loadAsync(vsixBuffer);
+    const manifestFile = vsixZip.file('extension/package.json');
+    if (manifestFile) {
+        const manifestContent = await manifestFile.async('string');
+        const manifest = JSON.parse(manifestContent);
+        if (manifest.extensionDependencies) {
+            for (const key in manifest.extensionDependencies) {
+                if (manifest.extensionDependencies.hasOwnProperty(key)) {
+                    const dependency = manifest.extensionDependencies[key];
+                    dependencies.push(dependency);
+                }   
             }
         }
+    }
+
+    return dependencies;
+}
+
+async function setupPlugins() {
+    try {
+        for (const vsixPath of vsixs.reverse()) {
+            // Install the plugin
+            console.log(`Installing plugin from: ${vsixPath}`);
+            await vscode.commands.executeCommand('workbench.extensions.installExtension', vscode.Uri.file(vsixPath));
+        }
+    } catch (error) {
+        throw new Error(`Plugin installation failed: ${error.message}`);
+    } finally {
+        // for (const vsixPath of vsixs) {
+        //     fs.unlinkSync(vsixPath);
+        // }
     }
 }
 
